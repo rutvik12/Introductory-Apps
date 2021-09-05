323f3cf63bf86300014066c5b0293eabfc285882
rmarakana3 <rmarakana3@gatech.edu>
2021-09-05 15:02:40 -0400

    [Added the addition functionality on back-end of the app]

--- AdditionApp/app/src/main/java/com/example/additionapp/MainActivity.java
+++ AdditionApp/app/src/main/java/com/example/additionapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.additionapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNum = findViewById(R.id.firstNum);
+                EditText secondNum = findViewById(R.id.secondNum);
+                EditText result = findViewById(R.id.result);
+
+                int num1 = Integer.parseInt(firstNum.getText().toString());
+                int num2 = Integer.parseInt(secondNum.getText().toString());
+                int sum = num1 + num2;
+                result.setText(sum +"");
+            }
+        });
     }
 }
